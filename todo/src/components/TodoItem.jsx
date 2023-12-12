836c267dde30f9908bb8d3324aa8c5e4085ed608
Milan5002 <milan.sana@kalvium.community>
2023-12-13 01:50:32 +0530

    done

---
+++ todo/src/components/TodoItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+export default class TodoItem extends React.Component{
+
+    render (){
+        console.log(this.props)
+        let {e,i, updateItem, deleteI} = this.props
+        return (
+            <div key={i}>
+            <input type="text" value={e} onChange={(event)=>{
+                updateItem(event.target.value, i)
+            }}/>
+            <button  onClick={()=>{
+                deleteI(i)
+            }}>Delete</button>
+            </div>
+        )
+    }
+
+}
